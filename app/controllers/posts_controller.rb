e0f824b15fd62df24f0c1f1078d52bfb80074cee
Matt Boldt <me@mattboldt.com>
2017-12-04 21:44:18 -0600

    adding posts model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.fetch(:post, {})
+      params.require(:post).permit(:title, :content)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,8 @@
 ActiveRecord::Schema.define(version: 20171205031557) do
 
   create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
